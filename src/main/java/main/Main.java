febd415c01fbc0d8939da3605f506990169377eb
Herry Pattar <fake_empire@mail.ru>
2016-05-27 05:57:02 +0300

    New application configs, Jersey ResourceConfig instead Application and new dependencies

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
 import db.services.AccountService;
+import db.services.impl.db.AccountDAO;
 import db.services.impl.db.DBAccountServiceImpl;
 import db.services.impl.db.DBSessionFactoryService;
 import game.services.MessagingService;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +25,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import server.messaging.socket.MessagingServlet;
 
+import javax.ws.rs.core.Application;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -34,29 +41,24 @@ public class Main {
     public static final String DEFAULT_PROP_PATH = "cfg/server.properties";
 
     @NotNull
-    static Properties loadProperties(Context serverContext) throws IOException {
+    static Configuration loadProperties(Context serverContext) throws Throwable {
         LOGGER.info(String.format("[ I ] Loading properties from path %s", DEFAULT_PROP_PATH));
-        FileInputStream in = null;
-        Properties props = null;
-        IOException savedThrowable = null;
+        Configuration props = null;
+        Throwable savedThrowable = null;
+        final Configurations configs = new Configurations();
         try {
-            in = new FileInputStream(DEFAULT_PROP_PATH);
-            props = new Properties();
-            props.load(in);
-        } catch (IOException t) {
-            LOGGER.error(String.format("[ E ] Something happened during configuration load:\n%s", t.toString()));
-            savedThrowable = t;
-        } finally {
-            if (in != null) try {
-                in.close();
-            } catch (IOException ignored) {}
+            props = configs.properties(DEFAULT_PROP_PATH);
+        } catch (ConfigurationException e) {
+            LOGGER.error(String.format("[ E ] Something happened during configuration load:\n%s", e.toString()));
+            savedThrowable = e;
         }
+
         if (props == null) {
             // rethrow exception
             throw savedThrowable;
         }
         LOGGER.info("[ I ] Loaded properties successfully!");
-        serverContext.put(Properties.class, props);
+        serverContext.put(Configuration.class, props);
         return props;
     }
 
@@ -65,15 +67,15 @@ public class Main {
     @Nullable
     static Server initializeServer(Context serverContext) {
         //final Configuration srvConfig=new Configuration("fdvdf");
-        final Properties serverProps;
+        final Configuration serverProps;
         try {
             serverProps = loadProperties(serverContext);
-        } catch (IOException e) {
+        } catch (Throwable e) {
             LOGGER.error(String.format("[ E ] Error when loading properties: %n%s", e.toString()));
             return null;
         }
-        final String address = serverProps.getProperty("server.address");
-        final String portString = serverProps.getProperty("server.port");
+        final String address = serverProps.getString("server.address");
+        final String portString = serverProps.getString("server.port");
         int port = 0;
         try {
             port = parseInt(portString);
@@ -108,25 +110,26 @@ public class Main {
         return resourceHandler;
     }
 
-    static void configureRestApi(ServletContextHandler contextHandler) {
-        final ServletHolder servletHolder = new ServletHolder(ServletContainer.class);
+    static void configureRestApi(Context serverContext, ServletContextHandler contextHandler) {
+        final ResourceConfig application = new RestAppV1(serverContext)
+                .register(JacksonFeature.class);
+        final ServletHolder servletHolder = new ServletHolder(new ServletContainer(application));
         //add our Context to ServletContextHandler
-        servletHolder.setInitParameter("javax.ws.rs.Application",RestAppV1.class.getCanonicalName());
         contextHandler.addServlet(servletHolder,"/api/v1/*");
     }
 
 
 
-    static void configureAccountService(Context restContext) {
+    static void configureAccountService(Context serverContext) {
         final DBSessionFactoryService factory = new DBSessionFactoryService();
         factory.configure();
-        final AccountService service = new DBAccountServiceImpl(factory);
-        restContext.put(AccountService.class , service);
-        restContext.put(DBSessionFactoryService.class, factory);
+        final AccountService service = new DBAccountServiceImpl(factory, new AccountDAO());
+        serverContext.put(AccountService.class , service);
+        serverContext.put(DBSessionFactoryService.class, factory);
     }
 
-    static void configureMessagingService(Context restContext, ServletContextHandler contextHandler) {
-        restContext.put(MessagingService.class, new MessagingService());
+    static void configureMessagingService(Context serverContext, ServletContextHandler contextHandler) {
+        serverContext.put(MessagingService.class, new MessagingService());
         final ServletHolder holderSockets = new ServletHolder("ws-events", MessagingServlet.class);
         contextHandler.addServlet(holderSockets, "/sockets/*");
     }
@@ -150,7 +153,7 @@ public class Main {
 
         configureAccountService(serverContext);
         configureMessagingService(serverContext, contextHandler);
-        configureRestApi(contextHandler);
+        configureRestApi(serverContext, contextHandler);
 
         configureServer(new Handler[]{resourceHandler, contextHandler}, server);
 
--- src/main/java/server/rest/RestAppV1.java
+++ src/main/java/server/rest/RestAppV1.java
@@ -1,8 +1,11 @@
 package server.rest;
 
 import db.services.AccountService;
+import game.services.MessagingService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
 import server.messaging.socket.MessagingServlet;
 
 import javax.servlet.ServletContext;
@@ -13,25 +16,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("/api/v1/")
-public class RestAppV1 extends Application {
+public class RestAppV1 extends ResourceConfig {
     private static final Logger LOGGER = LogManager.getLogger(RestAppV1.class);
 
-    final Set<Object> objects = new HashSet<>();
     server.Context context;
-    @Context
-    ServletContext ctx;
 
-    @Override
-    public Set<Object> getSingletons() {
-        context = (server.Context)ctx.getAttribute("context");
-        final AccountService accountService = context.get(AccountService.class);
-        final MessagingServlet messagingServlet = context.get(MessagingServlet.class);
-
+    public RestAppV1(server.Context context) {
         LOGGER.info("[+] Started application...");
-        objects.add(new UserServlet(accountService));
-        objects.add(messagingServlet);
-        objects.add(new SessionServlet(accountService));
-        //objects.add(new SignInServlet(accountService));
-        return objects;
+        this.context = context;
+        final AccountService accountService = context.get(AccountService.class);
+        register(new AbstractBinder(){
+            @Override
+            protected void configure() {
+                bind(context).to(server.Context.class);
+                bind(accountService).to(AccountService.class);
+            }
+        });
+        register(UserServlet.class);
+        register(SessionServlet.class);
     }
+
+
 }
